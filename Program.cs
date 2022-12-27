9c522fc21ca9e0eb7f749585ef82e1890cf12110
fferses <dans77772@gmail.com>
2022-12-27 17:29:54 +0400

    init rep

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Допит
+Console.Write(" Ваше ім'я?: ");
+var name = Console.ReadLine();
+Console.Write(" Ваше Призвіще?: ");
+var firstname = Console.ReadLine();
+Console.Write(" Ваш рік народження?: ");
+var year = Console.ReadLine();
+
+
+Console.Write(" Скільки ти витратив вчора?: ");
+double yesturdaypay = Convert.ToDouble(Console.ReadLine());
+Console.Write(" Скільки ти витратив сьогодні?: ");
+double todaypay = Convert.ToDouble(Console.ReadLine());
+Console.Clear(); 
+
+// Вивід анкети:
+Console.WriteLine($" {name} {firstname}");
+
+// перевірка повнолітності:
+DateTime useryear = new DateTime(int.Parse(year), 1, 1);
+if (DateTime.Today.AddYears(-18) > useryear)
+    Console.WriteLine (" Повнолітній");
+else
+    Console.WriteLine (" Неповнолітній");
+
+// Рахуємо суму витрат:
+double allpay = yesturdaypay + todaypay; 
+Console.WriteLine($" Сума витрат за два дні: {allpay}.");
+
+
+
+
+
+//Console.WriteLine($"Сумма витрат за два дні: {allpay} ");
+//Console.WriteLine("   Local date and time: {0}",
+//                           localDate.ToString());
+//Console.WriteLine(DateTime.UtcNow.Year);
+
