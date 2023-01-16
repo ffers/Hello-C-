d28e872896dde9a0a8b36d77aa58c54fe9d0c562
Dmytro Lytvynenko <dmytro.lytvynenko@fortius-network.com>
2023-01-16 12:26:27 +0200

    Test created

--- Program.cs
+++ Program.cs
@@ -1,35 +1,6 @@
-using helloapp;
+using MountainTest;
+using System;
 
+//MountainNavigator.RunNavigation();
 
-MountainNavigation.LetsStartNewJourney();
-int[,] map = MountainNavigation.GetThePlanetMap();
-(int x, int p) = MountainNavigation.GetShipPosition();
-
-bool finished = MountainNavigation.MoveTheSpaceShip(0, 0);
-
-while (finished == false)
-{
-    if (map[x - 1, p] == 0){
-        finished = MountainNavigation.MoveTheSpaceShip(-1, 0);
-        (x,p) = MountainNavigation.GetShipPosition();
-
-    }
-    else if (map[x, p - 1] == 0){
-        finished = MountainNavigation.MoveTheSpaceShip(0, -1);
-        (x,p) = MountainNavigation.GetShipPosition();
-    }
-    else if (map[x, p + 1] == 0) {
-                
-        finished = MountainNavigation.MoveTheSpaceShip(0, +1);
-        (x,p) = MountainNavigation.GetShipPosition();
-               if (map[x - 1, p] == 1)
-                    finished = MountainNavigation.MoveTheSpaceShip(0, +1);
-                    (x,p) = MountainNavigation.GetShipPosition();
-                    if (map[x - 1, p] == 1)
-                        finished = MountainNavigation.MoveTheSpaceShip(0, +1);
-                        (x,p) = MountainNavigation.GetShipPosition(); 
-                    
-                    
-            }
-}
-Console.WriteLine($"Road ship: {map[x-1,p]}");
+Test.TestNavigator();
